24854173311968047aaa8f6138d174faf92caa35
UlisesC12 <ulisescandela99@gmail.com>
2023-10-25 00:54:54 -0600

    Learn High Order Components by testing some code in Index.js of main page (Main Index.js)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return(
+    <h1>
+      {props.saludo}, {props.nombre}!
+    </h1>
+  )
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos Acompañando al wrapped component</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)("Buenas tardes")
+
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Ulises" />,
+  // <App saludo="Buenas" nombre="Nath"/>,
   document.getElementById('root')
 );
